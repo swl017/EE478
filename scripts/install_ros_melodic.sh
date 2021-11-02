572ab0f2784fdb1dba6edad6bd93f06ee0e0eee6
Seungwook Lee <36251439+swl017@users.noreply.github.com>
2021-11-02 15:26:32 +0900

    Update install_ros_melodic.sh

--- scripts/install_ros_melodic.sh
+++ scripts/install_ros_melodic.sh
@@ -53,7 +53,8 @@ echo "[Install the ros-desktop-full and all rqt plugins]"
 sudo apt-get install -y ros-$name_ros_version-desktop-full ros-$name_ros_version-rqt-*
 
 echo "[Initialize rosdep]"
-sudo sh -c "rosdep init"
+sudo apt install python-rosdep
+sudo rosdep init
 rosdep update
 
 echo "[Environment setup and getting rosinstall]"
